3f54b6af8cc5c86bb17d114a89738394a81c08b8
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-12 15:15:07 -0800

    Clean up RecordCtrl

--- public/javascripts/controllers/RecordCtrl.js
+++ public/javascripts/controllers/RecordCtrl.js
@@ -1,21 +1,8 @@
-// help from here
-// http://stackoverflow.com/questions/15014638/recorderjs-uploading-recorded-blob-via-ajax
-
 onceUpon.controller('RecordCtrl', function RecordCtrl($scope, SentencesFactory, $http) {
-    // Mirror the array of posts returned by the factory
-    // $scope.sentences = SentencesFactory.sentences;
-
     // recorder object must be scoped to the whole controller
     $scope.rec;
-    // $scope.config = {callback: $scope.blobCallback}
     $scope.context;
     $scope.mediaStreamSource;
-
-    // create a message var just to send info to the client
-    $scope.message;
-
-    $scope.object = new Object();
-    $scope.object.sendToServer = true;
 
     $scope.start = function() {
       $scope.rec.record();
@@ -28,19 +15,15 @@ onceUpon.controller('RecordCtrl', function RecordCtrl($scope, SentencesFactory, 
 
       $scope.rec.exportWAV(function blobCallback(blob) {
         $scope.rec.clear();
-        console.log('blob below:');
-        console.dir(blob);
 
+        // Read the blob as data url and send it to the backend w/ ajax
         var reader = new FileReader();
         reader.onload = function(event) {
-          // var arrayBuffSize = event.target.result.byteLength;
-          // console.log('arraybuffsize: ' + arrayBuffSize);
           $.ajax({
             type: 'POST',
             url: '/saveRecording',
             data: {
               audio: event.target.result,
-              // audio: new Uint8Array(event.target.result),
               text: $scope.text,
               timestamp: new Date()
             },
@@ -51,26 +34,20 @@ onceUpon.controller('RecordCtrl', function RecordCtrl($scope, SentencesFactory, 
           });
         }
         reader.readAsDataURL(blob);
-        // reader.readAsArrayBuffer(blob);
       });
     }
 
 
-
-    var successCallback = function (stream) {
+    // getUserMedia success and error callbacks
+    var gumSuccess = function (stream) {
       // Support various implementations of AudioContext
       $scope.context = new (window.AudioContext || window.webkitAudioContext)();
       $scope.mediaStreamSource = $scope.context.createMediaStreamSource(stream);
       $scope.rec = new Recorder($scope.mediaStreamSource);
-
-      // debugger;
     }
-
-    var errorCallback = function (err) {
+    var gumError = function (err) {
       console.log('The following getUserMedia error occured: ' + err);
     }
-
-
 
 
     // Initialize microphone when this partial is loaded
@@ -83,7 +60,7 @@ onceUpon.controller('RecordCtrl', function RecordCtrl($scope, SentencesFactory, 
 
       navigator.getUserMedia(
         {audio:true, video: false},
-        successCallback, errorCallback
+        gumSuccess, gumError
       );
     });
 });
