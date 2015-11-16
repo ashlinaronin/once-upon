83092f87e0112fdd9b2e59c6e7de3196b11eab95
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-15 23:33:07 -0800

    Import LAME library and copy example code from github.com/zhuker but yet to modify it to fit my needs

--- public/javascripts/services/SentencesFactory.js
+++ public/javascripts/services/SentencesFactory.js
@@ -5,6 +5,7 @@ onceUpon.factory('SentencesFactory', function SentencesFactory($http) {
   ** and receives JSON objects to the Express API which then does the
   ** actual database calls.
   */
+  var liblame = new lamejs();
 
   var factory = {};
   factory.sentences = [];
@@ -36,6 +37,51 @@ onceUpon.factory('SentencesFactory', function SentencesFactory($http) {
       }
       reader.readAsDataURL(blob);
     });
+  }
+
+  // from example at github.com/zhuker/lamejs
+  factory.encodeMonoMp3 = function(channels, sampleRate, samples) {
+    var buffer = [];
+    mp3enc = new liblame.Mp3Encoder(channels, sampleRate, 128);
+    var remaining = samples.length;
+    var maxSamples = 1152;
+    for (var i = 0; remaining >= maxSamples; i += maxSamples) {
+      var mono = samples.subarray(i, i + maxSamples);
+      var mp3buf = mp3enc.encodeBuffer(mono);
+      if (mp3buf.length > 0) {
+        buffer.push(new Int8Array(mp3buf));
+      }
+      remaining -= maxSamples;
+    }
+    var d = mp3enc.flush();
+    if (d.length > 0) {
+      buffer.push(new Int8Array(d));
+    }
+
+    console.log('done encoding mp3, size=', buffer.length);
+    var blob = new Blob(buffer, {type: 'audio/mp3'});
+    var bUrl = window.URL.createObjectURL(blob);
+    console.log('Blob created, URL:', bUrl);
+    // In the example, he puts the file here to play it, but we dont wanna
+  }
+
+  // from example at github.com/zhuker/lamejs
+  factory.loadWav = function() {
+    var wavFile = "test.wav";
+
+    var request = new XMLHttpRequest();
+    request.open("GET", wavFile, true);
+    request.responseType = "arraybuffer";
+
+    // our asynchronous callback
+    request.onload = function() {
+      audioData = request.response;
+      wav = liblame.Wavheader.readHeader(new DataView(audioData));
+      console.log('wav: ' wav);
+      samples = new Int16Array(audioData, wav.dataOffset, wav.dataLen / 2);
+      factory.encodeMp3(wav.channels, wav.sampleRate, samples);
+    }
+    request.send();
   }
 
   /* Get all sentences and deep copy them to update the factory. */
