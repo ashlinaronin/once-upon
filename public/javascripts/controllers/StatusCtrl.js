9e429fb943477808ecbc7db85329f32ab005f3e1
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-22 16:59:41 -0800

    Add color display for user status with ng-class

--- public/javascripts/controllers/StatusCtrl.js
+++ public/javascripts/controllers/StatusCtrl.js
@@ -1,6 +1,8 @@
 onceUpon.controller('StatusCtrl', function StatusCtrl($scope, StatusFactory) {
   $scope.userStatus = StatusFactory.userStatus;
 
+  $scope.statusColor = 'red';
+
   // Watch for changes to userStatus in StatusFactory service
   // Convoluted syntax is safer
   // We need both to watch scope here and apply it in service
@@ -8,6 +10,13 @@ onceUpon.controller('StatusCtrl', function StatusCtrl($scope, StatusFactory) {
     return StatusFactory.userStatus;
   }, function(newValue, oldValue) {
     $scope.userStatus = newValue;
+
+    // Color updating can be refactored, maybe into directive
+    if ($scope.userStatus === 'active') {
+      $scope.statusColor = 'active';
+    } else {
+      $scope.statusColor = 'waiting';
+    }
   });
 
 });
