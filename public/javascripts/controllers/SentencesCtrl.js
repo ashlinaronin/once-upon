b5ca4233f22e8fac341006cb2db956d4dd331b65
Ashlin Aronin <ashlinaronin@gmail.com>
2016-02-03 22:42:16 -0800

    Show different colored cursor when someone else is actively recording in the story

--- public/javascripts/controllers/SentencesCtrl.js
+++ public/javascripts/controllers/SentencesCtrl.js
@@ -4,4 +4,12 @@ onceUpon.controller('SentencesCtrl', function SentencesCtrl(
     $scope.SentencesFactory = SentencesFactory;
     $scope.PlaybackFactory = PlaybackFactory;
     $scope.SocketFactory = SocketFactory;
+
+    $scope.getCursorClass = function() {
+      if (SocketFactory.userPosition === 0) {
+        return "blinking-cursor-you";
+      } else {
+        return "blinking-cursor-friend";
+      }
+    }
 });
